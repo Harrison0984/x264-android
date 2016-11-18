687358e9cbbc867ad5380b2a003e3afbb6914d92
heyunpeng <heyunpeng@heyunpengdeMacBook-Pro.local>
2016-11-18 17:59:24 +0800

    first commit

---
+++ build_android_arm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PLATFORM=$ANDROID_NDK/platforms/android-18/arch-arm/
+TOOLCHAIN=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+PREFIX=./android/arm
+
+function build_one
+{
+  ./configure \
+  --prefix=$PREFIX \
+  --enable-static \
+  --enable-pic \
+  --host=arm-linux \
+  --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+  --sysroot=$PLATFORM
+
+  make clean
+  make
+  make install
+}
+
+build_one
+
+echo Android ARM builds finished
